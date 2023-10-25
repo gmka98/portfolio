0f7ca0c161ccf5405b8d9c4d6d6b1698f69de7d6
gmk4 <gmka98@gmail.com>
2023-10-25 08:59:25 +0200

    update cv

--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {motion, useScroll} from "framer-motion";
+import Link from "next/link";
 
 const LiIcon = ({reference}) =>{
 
@@ -11,6 +12,7 @@ const LiIcon = ({reference}) =>{
     )
     return(
         <figure className="absolute left-0 stroke-dark dark:stroke-light">
+           <Link href="/">
             <svg className="-rotate-90 md:w-[60px] md:h-[60px] xs:w-[40px] xs:h-[40px]" width="75" viewBox="0 0 100 100">
                 <circle cx="75" cy="50" r="20" className="stroke-primary dark:stroke-primaryDark stroke-1 fill-none" />
                 <motion.circle cx="75" cy="50" r="20" className=" stroke-[5px] fill-light dark:fill-dark"
@@ -19,6 +21,7 @@ const LiIcon = ({reference}) =>{
                 }} />
                 <circle cx="75" cy="50" r="10" className="animate-pulse stroke-1 fill-primary dark:fill-primaryDark" />
             </svg>
+            </Link>
         </figure>
     )
 
